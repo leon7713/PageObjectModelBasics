4b3e541cdbcc3a240b321e01fd23876940642611
leon7713 <leonushar@gmail.com>
2019-03-15 17:44:50 +0200

    commenting popup

--- src/main/java/com/w2a/pages/LoginPage.java
+++ src/main/java/com/w2a/pages/LoginPage.java
@@ -1,7 +1,6 @@
 package com.w2a.pages;
 
 import com.w2a.base.Page;
-import org.openqa.selenium.By;
 
 import java.io.IOException;
 
@@ -20,9 +19,9 @@ public class LoginPage extends Page {
 //            driver.findElement(By.cssSelector(".close-btn")).click();
 //        }
 
-        if (isElementPresent(By.cssSelector(OR.getProperty("closebtn_CSS")))) {
-            click("closebtn_CSS");
-        }
+//        if (driver.findElement(By.cssSelector(OR.getProperty("closebtn_CSS"))).isDisplayed()) {
+//            click("closebtn_CSS");
+//        }
 
         return new ZohoAppPage();
     }
